098160082da07889a30558129dcc44fab063b791
Philippe <Philippe@DESKTOP-3T3ABSL>
2016-05-24 09:33:35 +0200

    Add project files.

---
+++ RomanKataArabicToRoman/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanKataArabicToRoman
+{
+    public class Class1
+    {
+    }
+}
