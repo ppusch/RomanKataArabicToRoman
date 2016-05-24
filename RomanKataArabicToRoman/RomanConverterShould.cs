b4162ecb0f08cbbc68354031862af8bba57d260a
Philippe <Philippe@DESKTOP-3T3ABSL>
2016-05-24 10:26:40 +0200

    Red Cycle for First Test

--- RomanKataArabicToRoman/RomanConverterShould.cs
+++ RomanKataArabicToRoman/RomanConverterShould.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NFluent;
+using NUnit.Framework;
 
 namespace RomanKataArabicToRoman
 {
     public class RomanConverterShould
     {
+        [Test]
+        public void return_I_when_Arabic_Number_is_1()
+        {
+            var romanConverter = new RomanConverter();
+            string romanSymbol = romanConverter.ArabicToRoman(1);
+            Check.That(romanSymbol).IsEqualTo(1);
+
+        }
     }
 }
